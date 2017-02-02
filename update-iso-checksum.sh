eb6a5d50df35a61272fcabf3da48f7820c1c0671
Bramford Horton <bram.horton@gmail.com>
2017-02-02 22:31:31 +0000

    Fix binary checks in update-iso-checksum.sh

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -2,7 +2,8 @@
 
 # Downloads the latest iso checksum and updates the variables in the json template
 which jq || { echo "ERROR: 'jq' not found in PATH"; exit 1; }
-which wget || { echo "ERROR: 'wget' not found in PATH"; exit 1; }
+which curl || { echo "ERROR: 'curl' not found in PATH"; exit 1; }
+which sed || { echo "ERROR: 'sed' not found in PATH"; exit 1; }
 iso_checksum_type="$(jq --raw-output '.variables.iso_checksum_type' packer-debian9-qemu-kvm.json)"
 iso_file_url="$(jq --raw-output '.variables.remote_mirror_url' packer-debian9-qemu-kvm.json)"
 iso_file_name="$(jq --raw-output '.variables.iso_name' packer-debian9-qemu-kvm.json)"
