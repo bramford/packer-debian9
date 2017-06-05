b1936a44eaa79554246b4131fa3b5b6f8d9aed52
Bramford Horton <bram.horton@gmail.com>
2017-06-05 14:09:17 +1200

    Add script to automate checksum update

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
 
 # Downloads the latest iso checksum and updates the variables in the json template
+
+template_filename="packer-debian9-qemu-kvm.json"
+
 which jq || { echo "ERROR: 'jq' not found in PATH"; exit 1; }
 which curl || { echo "ERROR: 'curl' not found in PATH"; exit 1; }
 which sed || { echo "ERROR: 'sed' not found in PATH"; exit 1; }
-iso_checksum_type="$(jq --raw-output '.variables.iso_checksum_type' packer-debian9-qemu-kvm.json)"
-iso_file_url="$(jq --raw-output '.variables.remote_mirror_url' packer-debian9-qemu-kvm.json)"
-iso_file_name="$(jq --raw-output '.variables.iso_name' packer-debian9-qemu-kvm.json)"
+iso_checksum_type="$(jq --raw-output '.variables.iso_checksum_type' ${template_filename})"
+iso_file_url="$(jq --raw-output '.variables.remote_mirror_url' ${template_filename})"
+iso_file_name="$(jq --raw-output '.variables.iso_name' ${template_filename})"
 new_iso_checksum=$(curl -sq ${iso_file_url}/$(echo ${iso_checksum_type} | awk '{print toupper($0)}')SUMS  | grep ${iso_file_name} | awk '{ print $1 }')
-sed -i packer-debian9-qemu-kvm.json -r -e "s/(\"iso_checksum\"\:\ \")\w{128}(\",)/\1${new_iso_checksum}\2/g"
+sed -i ${template_filename} -r -e "s/(\"iso_checksum\"\:\ \")\w{128}(\",)/\1${new_iso_checksum}\2/g"
